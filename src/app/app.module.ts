89c47c624c197f1e77e99954d54385ba5e59ca8c
Felipe Teofilo Siqueira Costa <felipeteofilosiqueiracosta@gmail.com>
2023-04-29 16:54:46 -0300

    finishing test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,18 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ItemListComponent } from './item-list/item-list.component';
 
 import { NgxBootstrapIconsModule } from 'ngx-bootstrap-icons';
-import { pencil, trash, personPlusFill } from 'ngx-bootstrap-icons';
+import { pencil, trash, personPlusFill, search } from 'ngx-bootstrap-icons';
+import { InputComponent } from './input/input.component';
 
 const icons = {
   pencil,
   trash,
   personPlusFill,
+  search,
 };
 
 @NgModule({
-  declarations: [AppComponent, ItemListComponent],
+  declarations: [AppComponent, ItemListComponent, InputComponent],
   imports: [BrowserModule, NgbModule, NgxBootstrapIconsModule.pick(icons)],
   providers: [],
   bootstrap: [AppComponent],

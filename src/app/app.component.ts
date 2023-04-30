f2c7f08d65ae9674dca83be0c36067e6935ffbd9
Felipe Teofilo Siqueira Costa <felipeteofilosiqueiracosta@gmail.com>
2023-04-30 09:46:36 -0300

    feat: search by email input on mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ItemProps } from './item-list/item-list.component';
 import { DeviceDetectorService } from 'ngx-device-detector';
 @Component({
@@ -6,7 +6,8 @@ import { DeviceDetectorService } from 'ngx-device-detector';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  allItems: ItemProps[] = [];
   items: ItemProps[] = [];
   pageSize: number = 5;
   page: number = 1;
@@ -14,13 +15,25 @@ export class AppComponent {
 
   constructor(private deviceService: DeviceDetectorService) {
     for (let i = 0; i < 1000; i++) {
-      this.items.push({
+      this.allItems.push({
         email: `Sample email ${i}`,
         games: `Game 1, Game 2, Game 3, Game 4`,
         id: i,
         name: `Sample name ${i}`,
       });
     }
+    this.items = this.allItems;
+
     this.isVisible = deviceService.isMobile();
+  }
+  searchByEmail(email: string) {
+    console.log(email);
+    const result = this.allItems.filter((item) => item.email === email);
+
+    this.items = result;
+    if (email == '') this.items = this.allItems;
+  }
+  ngOnInit(): void {
+    console.log('iniciou!');
   }
 }
--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-input',
@@ -7,4 +7,9 @@ import { Component, Input } from '@angular/core';
 })
 export class InputComponent {
   @Input() isVisible = false;
+  @Output() emailSearched = new EventEmitter<string>();
+
+  handleSearchButton(email: string) {
+    this.emailSearched.emit(email);
+  }
 }
